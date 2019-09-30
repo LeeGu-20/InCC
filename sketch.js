0c406022dcf62d509b28e9db66cfd55a1cd2b6bc
LeeGu-20 <55528389+LeeGu-20@users.noreply.github.com>
2019-09-30 23:53:09 +0900

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,44 +1,69 @@
-//load the image of the application
+//background image
 function preload() {
-  background=loadImage("background.png")}
-
-
-var cx = [];
-var cy = [];
-var N,i;
-
+  backimage = loadImage("sky.png")
+}
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
-  frameRate(60);
-  image(background, 0,0, windowWidth,windowHeight);
-  createBackstars();
+  createCanvas(windowWidth, windowHeight);
+  image(backimage,0,0,windowWidth, windowHeight);
 }
-
-
 
 function draw() {
-  //set the mainpaint brush
-  if (mouseIsPressed) {
-    var c = color(0,255,255);
-    fill(c);
-    ellipse(mouseX,mouseY,15,15);}
+  
+  randomSeed(0);
+  
+  var x,y,r;
+  var delta = 30;
+  
+  var starsPosition = map(mouseX, 0, windowWidth, 0,1);
+  var starsColor_Weight = map(mouseY, 0, windowHeight,0,100);
+  
+  
+  switch(mode) {
+  //falling stars
+  case 'line' : 
+      
+  for (y=0; y<windowHeight; y+=delta) {
+    for(x=0; x<windowWidth; x+=delta) {
+      r=random(0,1);
+      if (r<starsPosition) {
+        storke(255,90,starsColor_Weight);
+        strokeWeight(starsColor_Weight);
+       line(x,y,x+delta,y+delta);
+      }else {
+        stroke(starsColor_Weight,255,90);
+        storkeWeight(starsColor_Weight);
+        line(x,y,x+delta,y+delta);}}}
+  break;
+  
+  //moving planets
+  case 'ellipse' : 
+  
+  for (y=0; y<windowHeight; y+=delta) {
+    for (x=0; x<windowWidth; x+=delta) {
+      r=random(0,1);
+      if (r<0.5) {
+        fill(0,0,r);
+        ellipse(x,y,delta,delta);
+    break;}}}}}
+
+
+//change mode
+function keyPressed() {
+  switch(key) {
+    case 's' : mode = "line";
+    break;
+    case 'p' : mode = "ellipse";
+    break;
+    default : mode= "none"
+    break;
+  }
+  print(mode);
 }
 
 
 
-// setting background stars
-function createBackstars() {
-  N=100;
-  //determine the position randomly
-  for (i=0; i<N; i++) {
-    cx[i] = random(0,width);
-    cy[i] = random(0,height);
-    
-    //setting their color and size randomly
-    var sColor = random(0,255);
-    fill(255, 255, sColor);
-    noStroke();
-    var sSize = random(2,7);
-    ellipse(cx[i],cy[i],sSize,sSize);}   
-}
+
+
+
+
